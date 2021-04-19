1174539cb77a3d2096471b0c376e8cb3540b7f09
Aryanjain <77156045+aryanjain19@users.noreply.github.com>
2021-04-19 22:29:51 +0530

    Delete Matrix add.py

--- RL/Aryan Jain/Matrix add.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-a=int(input("number of rows in matrix= "))
-b=int(input("no. of cols in matrix = "))
-m1=np.random.rand(a,b)
-m2=np.random.rand(a,b)
-ans=m1+m2
-print("sumation f the 2 matrix is =\n",ans)
