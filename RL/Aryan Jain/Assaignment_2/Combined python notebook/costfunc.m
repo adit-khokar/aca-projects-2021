21286105d2d7f6c7db7cd57181f1a36e51097957
sandeepb20 <sandeepskr1721@gmail.com>
2021-06-25 00:07:56 +0530

    assignment 3

--- RL/Aryan Jain/Assaignment_2/Combined python notebook/costfunc.m
+++
@@ -1,7 +0,0 @@
-function J=costfunc(x,y,theta)
-m=length(y);
-h=x*theta;
-dif=(h-y).^2;
-s=sum(dif);
-J=s/(2*m);
-end
--- RL/Aryan Jain/Assaignment_2/Combined python notebook/sigmoid.m
+++
@@ -1,4 +0,0 @@
-function g=sigmoid(x)
-g=zeros(size(x));
-g=1./(exp(-x)+1);
-end
